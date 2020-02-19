931dcf4f2bf490f71b99908b2cd13847f70714e0
JohnHG1 <honisz@yahoo.com>
2020-02-19 17:19:14 +0000

    MVP complete

--- models/traveller.js
+++ models/traveller.js
@@ -2,21 +2,35 @@ const Traveller = function(journeys) {
   this.journeys = journeys;
 };
 
-Traveller.prototype.getJourneyStartLocations = function() {
 
+Traveller.prototype.getJourneyStartLocations = function(){
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
 };
 
-Traveller.prototype.getJourneyEndLocations = function () {
 
+Traveller.prototype.getJourneyEndLocations = function(){
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
 };
 
-Traveller.prototype.getJourneysByTransport = function (transport) {
-
+Traveller.prototype.getJourneysByTransport = function (transport){
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
-Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+Traveller.prototype.getJourneysByMinDistance = function (minDistance){
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
 };
+
+
+
+
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
 
