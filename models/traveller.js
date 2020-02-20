736af97e6cd760c5ac9c729cab2f4e989ffbe023
JohnHG1 <honisz@yahoo.com>
2020-02-20 10:10:47 +0000

    edits after consultation

--- models/traveller.js
+++ models/traveller.js
@@ -8,6 +8,15 @@ const Traveller = function(journeys) {
     });
   };
 
+// Traveller.prototype.getJourneyStartLocations = function(){
+  //   const results = this.journeys.map((journey) => {
+  //     return journey.startLocation
+  //   })
+  //   return results
+  // };
+  // ## optional feedback code
+
+
   Traveller.prototype.getJourneyEndLocations = function (){
     return this.journeys.map((journey) => {
       return journey.endLocation;
@@ -19,6 +28,12 @@ const Traveller = function(journeys) {
       return journey.transport === transport;
     })
   };
+// Traveller.prototype.getJourneyByTransport = function (transport) {
+    //   return this.journeys.filter((journey) => {
+    //     return transport === journey.transport;
+    //   });
+    // };
+    // ## optional feedback code
 
   Traveller.prototype.getJourneysByTransport = function (transport){
     return this.journeys.filter((journey) => {
@@ -31,25 +46,47 @@ const Traveller = function(journeys) {
       return journey.distance > minDistance;
     })
   };
+// Traveller.prototype.getJourneysByMinDistance = function (minDistance){
+      // return this.journeys.filter((journey) => {
+  //     return min.distance > minDistance;
+  //   });
+  // };
+  // ## optional feedback code
+
+
 
   Traveller.prototype.calculateTotalDistanceTravelled = function (){
     return this.journeys.reduce((totalDistance, journey) => {
       return totalDistance += journey.distance;
     }, 0);
   };
+  // Traveller.prototype.calculateTotalDistanceTravelled = function (){
+      //return this.journeys.reduce((total, journey) => {
+      //    return total + journey.distance;
+      //  }, 0)
+      //);
+      // ## optional feedback code
 
 
-//   Traveller.prototype.getUniqueModesOfTransport = function () {
-//     return this.journeys.every((journey) => {
-//       return
-//     });
-// };
-// _.uniq( array, [isSorted], [iteratee] )
-// 
-// Array.prototype.getUniqueModesOfTransport= function() {
-//   return this.filter(function ((transport)) {
-//     return transport.indexOf(value) === index;
-//   });
-// };
+
+  Traveller.prototype.getUniqueModesOfTransport = function (){
+    const transports = this.journeys.map((journey) => {
+        return journey.transport;
+      });
+        return transports.filter((transport, index) => {
+          return transports.indexOf(transport) === index;
+          });
+        };
+
+  // Traveller.prototype.getUniqueModesOfTransport = function () {
+  //   return this.journeys.map((journey) => {
+  //     return journey.transport;
+  //   })
+  //     .filter((transport, index, array) => {
+  //       return array.indexOf(transport) === index;
+  //     });
+  // };
+  // ## optional feedback code
+
 
 module.exports = Traveller;
